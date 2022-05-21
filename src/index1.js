ff9d6ba7e54c5124fcc3207d8acf495d41257b05
Danila <shestakovsky-vladimir@yandex.ru>
2022-05-21 21:51:09 +0300

    ok

--- src/index1.js
+++ src/index1.js
@@ -1,33 +1,3 @@
-import './css/index.css';
-
-
-let offsett = 0;
-const sliderLine = document.querySelector('.slider-line');
-
-document.querySelector('.slider-next').addEventListener('click', function(){
-    offsett = offsett + 256;
-    document.querySelector('.slider-prev').style.background = "white";
-      document.querySelector('.slider-prev').disabled = false;
-    if (offsett >= 768) {
-      document.querySelector('.slider-next').style.background = "orange";
-      document.querySelector('.slider-next').disabled = true
-    } 
-    sliderLine.style.left = -offsett + 'px';
-});
-
-document.querySelector('.slider-prev').addEventListener('click', function () {
-    offsett = offsett - 256;
-    document.querySelector('.slider-next').style.background = "white";
-      document.querySelector('.slider-next').disabled = false;
-    if (offsett <= 0) {
-      document.querySelector('.slider-prev').style.background = "orange";
-      document.querySelector('.slider-prev').disabled = true;
-    }
-    sliderLine.style.left = -offsett + 'px';
-});
-
-//calulator
-
 var keys = document.querySelectorAll('#adding_machine span');
 var operators = ['+', '-', 'x', '÷'];
 var decimalAdded = false;
